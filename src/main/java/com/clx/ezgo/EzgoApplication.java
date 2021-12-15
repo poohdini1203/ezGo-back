4ade06e0b44330af456b65e1b94a46ef3cb7fffe
poohdini1203 <939015806@qq.com>
2021-12-15 20:52:52 +0800

    tangG

--- src/main/java/com/clx/ezgo/EzgoApplication.java
+++ src/main/java/com/clx/ezgo/EzgoApplication.java
@@ -1,9 +1,12 @@
-package com.example.ezgo;
+package com.clx.ezgo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan("com.clx.ezgo.mapper")
 public class EzgoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/clx/ezgo/controller/DriverController.java
+++ src/main/java/com/clx/ezgo/controller/DriverController.java
@@ -1,66 +1,79 @@
 package com.clx.ezgo.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.clx.ezgo.entity.DriverInfo;
+import com.clx.ezgo.service.DriverService;
+import com.clx.ezgo.utils.UploadUtils;
+
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.io.File;
-import java.util.Iterator;
-import java.util.UUID;
+import java.io.*;
+import java.util.List;
 
+@Slf4j(topic = "dirver")
 @RestController
-@RequestMapping("/upload")
-public class UploadController {
-    private static final Logger logger= LoggerFactory.getLogger(UploadController.class);
-    @RequestMapping("/picture")
-    public void uploadPicture(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        request.setCharacterEncoding("utf-8"); //设置编码
-
-        String id = request.getParameter("openID");
-        String category=request.getParameter("cat");
-        String filePath = "";
-        String relativePath="/driver/"+id+"/"+category+"/";
-        String realPath = request.getSession().getServletContext().getRealPath(relativePath );
-        File dir = new File(realPath);
-
-        //文件目录不存在，就创建一个
-        if (!dir.isDirectory()) {
-            dir.mkdirs();
-        }
-
-            StandardMultipartHttpServletRequest req = (StandardMultipartHttpServletRequest) request;
-            //获取formdata的值
-            Iterator<String> iterator = req.getFileNames();
-
-            while (iterator.hasNext()) {
-                MultipartFile file = req.getFile(iterator.next());
-                String fileName = file.getOriginalFilename();
-                //真正写到磁盘上
-                String uuid = UUID.randomUUID().toString().replace("-", "");
-                String kzm = fileName.substring(fileName.lastIndexOf("."));
-                String filename = uuid + kzm;
-                File file1 = new File(realPath + filename);
-                OutputStream out = new FileOutputStream(file1);
-                out.write(file.getBytes());
-                out.close();
-                filePath = request.getScheme() + "://" +
-                        request.getServerName() + ":"
-                        + request.getServerPort()
-                        + relativePath+filename;
-                System.out.println("访问图片路径:" + filePath + "   id:" + id);
-                System.out.println(realPath);
-            }
+@RequestMapping("/driver")
+public class DriverController {
+    private static final Logger logger= LoggerFactory.getLogger(DriverController.class);
 
 
+    @Autowired
+    DriverService driverService;
 
+    @RequestMapping("/addDriver")
+    public void addDriver(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        //设置编码
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        String openid=request.getParameter("openID");
+        String model=request.getParameter("model");
+        String plate=request.getParameter("plateNumber");
+        String color=request.getParameter("color");
+        String res=driverService.addDriver(openid,model,plate,color);
+
+        JSONObject result=new JSONObject();
+        result.put("result",res);
+        PrintWriter printWriter=response.getWriter();
+        printWriter.write(result.toJSONString());
+
+        log.info("addDriver");
     }
 
+    @RequestMapping("/uploadPicture")
+    public void uploadPicture(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //设置编码
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        String openid=request.getParameter("openID");
+        String category=request.getParameter("cat");
+        String filePath=UploadUtils.uploadDriverImg(openid,category,request);
+        driverService.uploadImg(openid,category,filePath);
+
+        log.info(filePath);
+    }
+
+    @RequestMapping("/getDriverInfoByID")
+    public void getDriverInfoByID(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //设置编码
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        String openid=request.getParameter("openID");
+        DriverInfo driverInfo=driverService.getDriverInfoByID(openid);
+        JSONObject result=new JSONObject();
+        result.put("driverInfo",driverInfo);
+        PrintWriter printWriter=response.getWriter();
+        printWriter.write(result.toJSONString());
+        log.info("driverInfo");
+    }
 }
